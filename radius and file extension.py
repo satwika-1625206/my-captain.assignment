50721d4eb851a16a5eb6eddce615e3ceade199b8
satwika-1625206 <103840748+satwika-1625206@users.noreply.github.com>
2022-04-30 21:19:19 +0530

    Add files via upload

---
+++ radius and file extension.py
@@ -0,0 +1,15 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def area_of_circle(r):
+        pi=3.1415926535897
+        area=pi*r*r
+        return area
+
+    
+print(area_of_circle(1.1))
+3.8013271108435376
+filename=input("Input the file name:abc.py")
+Input the file name:abc.py
+f_extns=filename.split(".")
+print("The extension of the file is:python")
+The extension of the file is:python
